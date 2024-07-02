a6bd0055e4421e38135391a3998571038ea228d0
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-02 23:30:05 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
 # Using Puppet to make changes to our configuration file.
 
-file {'/etc/ssh/ssh_config':
-	ensure => 'present',
+file { '/etc/ssh/ssh_config':
+	ensure	=> 'file',
+	owner	=> 'root',
+	group	=> 'root',
+	mode	=> '0644',
 }
-file_line {'Turn off passwd auth':
+
+file_line { 'Turn off passwd auth':
 	path	=> '/etc/ssh/ssh_config',
 	line	=> 'PasswordAuthentication no',
-	match	=> 'PasswordAuthentication yes',
-	replace	=> 'true',
+	match	=> '^#?PasswordAuthentication',
 }
-file_line {'Declear identity file':
+
+file_line { 'Declare identity file':
 	path	=> '/etc/ssh/ssh_config',
 	line	=> 'IdentityFile ~/.ssh/school',
-	match	=> '^IdentityFile',
-	ensure	=> 'present',
+	match	=> '^#>IdentityFile',
 }
