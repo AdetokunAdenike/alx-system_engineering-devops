beb71d5d47313131fcf2f3707a1eb75d2117da11
AdetokunAdenike <adetokunadenike@gmail.com>
2024-07-30 01:51:57 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,24 +9,23 @@ import sys
 
 
 if __name__ == "__main__":
-    # Base URL for the JSONPlaceholder API
+    # The Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Get the employee information using the provided employee ID
+    # Using the provided employee ID, get the employee information
     employee_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    # Get the to-do list for the employee using the provided employee ID
+    # Using the provided employee's ID, get the to-do list for the employee
     params = {"userId": employee_id}
     todos = requests.get(url + "todos", params).json()
 
-    # Filter completed tasks and count them
+    # Filter and count completed tasks
     completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    # Print the employee's name and the number of completed tasks
+    # Print the employee's name with corresponding number of completed tasks
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
 
-    # Print the completed tasks one by one with indentation
+    # Print the completed tasks one after the other with indentation
     [print("\t {}".format(complete)) for complete in completed]
-
