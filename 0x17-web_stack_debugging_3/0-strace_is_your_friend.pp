8116a00d0e5d821e079392c20124560bbd37f5e5
AdetokunAdenike <adetokunadenike@gmail.com>
2024-08-15 03:09:00 +0100

    Update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,14 @@
-# This Puppet manifest ensures the Apache log directory exists with the correct permissions
+# This Puppet manifest ensures the Apache log
+# directory exists with the correct permissions
 
-{ 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+file { '/var/log/apache2':
+  ensure => directory,
+  owner  => 'root',
+  group  => 'adm',
+  mode   => '0755',
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
