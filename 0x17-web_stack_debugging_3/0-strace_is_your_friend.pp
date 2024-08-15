162e92b1e68e0dad7855963f6461b98cd1aa0e82
AdetokunAdenike <adetokunadenike@gmail.com>
2024-08-15 02:09:41 +0100

    Update

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,13 @@
+# This Puppet manifest ensures the Apache log directory exists with the correct permissions
+
+file { '/var/log/apache2':
+  ensure => directory,
+  owner  => 'root',
+  group  => 'adm',
+  mode   => '0755',
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
