252640cae0c9e6001128c6b626c773f9e89eb004
AdetokunAdenike <adetokunadenike@gmail.com>
2024-08-22 23:52:25 +0000

    Fix: Fix nginx configuration

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# fix nginx
+# Fix nginx configuration settings
 
 exec {'modify max open files limit setting':
    command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
