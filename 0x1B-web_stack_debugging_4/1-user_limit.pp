095ef9ddad7dadeb01518698008313bd4084f1cc
AdetokunAdenike <adetokunadenike@gmail.com>
2024-08-23 00:32:46 +0000

    Update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,13 @@
+# Increase the file descriptor limits for the holberton user to prevent "Too many open files" errors.
+
 # Increase the file descriptor limits for the holberton user to prevent "Too many open files" errors
 
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/^holberton.*hard.*nofile/ s/[0-9]\\+$/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin',
 }
 
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/^holberton.*soft.*nofile/ s/[0-9]\\+$/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin',
 }
-
