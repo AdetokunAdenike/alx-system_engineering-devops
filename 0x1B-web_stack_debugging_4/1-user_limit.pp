0f71e0b27679e828479b075a318968ca4140dac8
AdetokunAdenike <adetokunadenike@gmail.com>
2024-08-23 00:13:25 +0000

    Update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,13 @@
-# Reconfigure the OS for 'holberton' to login and open a file without an error message.
+# Increase the file descriptor limits for the holberton user to prevent "Too many open files" error.
 
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/^holberton.*hard.*nofile/ s/[0-9]\\+$/50000/" /etc/security/limits.conf || echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin',
+  onlyif  => 'grep -q "^holberton.*hard.*nofile" /etc/security/limits.conf',
 }
 
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'increase-soft-file-limit-holberton-user':
+  command => 'sed -i "/^holberton.*soft.*nofile/ s/[0-9]\\+$/50000/" /etc/security/limits.conf || echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin:/usr/bin:/usr/sbin:/sbin',
+  onlyif  => 'grep -q "^holberton.*soft.*nofile" /etc/security/limits.conf',
 }
